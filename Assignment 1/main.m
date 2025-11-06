4785c7577d9ff620d04e2a0688b5d268aafe7ba9
gabrieleefrigerio <gabrielefrigerio@me.com>
2025-11-06 15:43:05 +0100

    LQR assignment 1

--- Assignment 1/main.m
+++ Assignment 1/main.m
@@ -13,6 +13,7 @@ clc
 clear; close all; clc;
 
 %% ------------------------ Parametri del modello -------------------------
+global m Cd
 m   = 1;          % massa del corpo [kg]
 Cd  = 0.15;       % coefficiente di resistenza aerodinamica (attrito quadratico)
 t0  = 0;          % tempo iniziale [s]
@@ -161,3 +162,153 @@ title('Traiettoria finale dopo ottimizzazione');
 % (plot opzionale per visualizzare la convergenza del costo)
 % X = 1 : ii;
 % semilogy(X, J(X,1))
+
+%% LQR Regulator
+% Re-sample the optimal trajectory 
+
+% Time discretization in N = number of time intervals
+N = 5000;
+
+Ns = size(Z,2); % number of states
+Nu = size(u,1); % number of controls
+
+% Sampled time history
+T = linspace(0,Tz(end),N);
+
+% Interpolation/sampling of the state and control 
+zk = zeros(N,Ns);
+uk = zeros(N,Nu);
+for ii = 1:Ns
+    zk(:,ii) = interp1(Tz,Z(:,ii),T);
+end
+for ii = 1:Nu
+    uk(:,ii) = interp1(Tu(1:end),u(ii,:),T);
+end
+
+% Option for ODE solver
+VectTol = ones(Ns^2,1)*1e-5;
+options = odeset('RelTol', 1e-5, 'AbsTol',VectTol);
+
+% initial and final time
+t0 = T(1);
+tf = T(end);
+
+% boundary conditions
+p0 = P(1:end)';
+
+% Integration of the matrix riccati equation
+[Tp,PP] = ode23(@(t,p) DRE(t,p,Q,R,zk,T,uk,T), flip(T), p0, options);
+
+% From backward to forward dynamics (they are stored in the reversed order)
+PP = flipud(PP);
+Tp = flipud(Tp);
+
+% Transformation Vector -> Matrix
+PP_Matrix = zeros(Ns,Ns);
+
+% Computation of the gain matrix in time, Uncontrolled stability matrix,
+% Controlled stability matrix along the trajectory
+K = zeros(N,Nu,Ns);
+A = zeros(N,Ns,Ns);
+Fc = zeros(N,Ns,Ns);
+PolesUC = zeros(N,Ns);
+PolesC = zeros(N,Ns);
+Xp = zeros(N,Ns);
+for ii = 1:N % Note: try also reshape.m
+    % transformation vector -> matrix
+    PP_Matrix(1:end) = PP(ii,:)';
+    % control matrix G
+    B = [0 0; 0 0; 0 1; 1/m 0];
+    % Uncontrolled state stability matrix
+    A(ii,:,:) = [0 0 -zk(ii,4)*sin(zk(ii,3))  cos(zk(ii,3));
+         0 0  zk(ii,4)*cos(zk(ii,3))  sin(zk(ii,3));
+         0 0  0           0;
+         0 0  0       -2*Cd*zk(ii,4)/m];
+    % Uncontrolled system poles
+    PolesUC(ii,:) = eig(squeeze(A(ii,:,:)));
+    % Gain matrix C
+    K(ii,:,:) = R^-1*B'*PP_Matrix; % R has to be non-null
+    % Controlled state stability matrix
+    Fc(ii,:,:) = squeeze(A(ii,:,:)) - B*squeeze(K(ii,:,:));
+    % Controlled system poles
+    PolesC(ii,:) = eig(squeeze(Fc(ii,:,:)));    
+end
+
+FigTag = figure;
+FigPos = FigTag.Position;
+FigTag.Position = [FigPos(1)-FigPos(3)*0.25,FigPos(2)-FigPos(4)*0.2,FigPos(3)*1.5,FigPos(4)*1.2];
+filename = 'Trajectory.gif';
+PlotSpeed = 20;
+for ii = 1:N/PlotSpeed
+    
+    clf(FigTag)
+    
+    % Temporal evolution - uncontrolled system
+    ax = subplot(2,2,1); hold on; grid on;
+    plot(real(PolesUC(ii*PlotSpeed,:)),imag(PolesUC(ii*PlotSpeed,:)),'x',...
+                'LineWidth',2,'Color','k'); hold on; box on;
+    eigRe = real(PolesUC(1:ii*PlotSpeed,:));
+    eigIm = imag(PolesUC(1:ii*PlotSpeed,:));
+    Tmatrix = Tp(1:ii*PlotSpeed)*ones(1,size(eigRe,2));
+    scatter(eigRe(:),eigIm(:),2,Tmatrix(:),'fill'); hold on; box on;
+    colormap jet; caxis([0,Tp(end)]);
+    title('Uncontrolled system','Interpreter','LaTex')
+    xlabel('$\Re\{\lambda\}$ [Rad/s]','Interpreter','LaTex');
+    ylabel('$\Im\{\lambda\}$ [Rad/s]','Interpreter','LaTex');
+    ax.FontSize = 14;
+    ax.TickLabelInterpreter = 'LaTex';
+    ylim([-25,25])
+    xlim([-50,10])
+    
+    % Temporal evolution - controlled system
+    ax = subplot(2,2,2); hold on; grid on;
+    plot(real(PolesC(ii*PlotSpeed,:)),imag(PolesC(ii*PlotSpeed,:)),'x',...
+                'LineWidth',2,'Color','k'); hold on; box on;
+    eigRe = real(PolesC(1:ii*PlotSpeed,:));
+    eigIm = imag(PolesC(1:ii*PlotSpeed,:));
+    Tmatrix = Tp(1:ii*PlotSpeed)*ones(1,size(eigRe,2));
+    scatter(eigRe(:),eigIm(:),2,Tmatrix(:),'fill'); hold on; box on;
+    colormap jet;  caxis([0,Tp(end)]);
+    title('Controlled system','Interpreter','LaTex')
+    xlabel('$\Re\{\lambda\}$ [Rad/s]','Interpreter','LaTex');
+    ylabel('$\Im\{\lambda\}$ [Rad/s]','Interpreter','LaTex');
+    ax.FontSize = 14;
+    ax.TickLabelInterpreter = 'LaTex';
+    ylim([-25,25])
+    xlim([-50,10])
+
+    
+    % plot of the trajectory
+    ax = subplot(2,2,[3,4]);
+    hold on; grid on;
+    title('Trajectory','Interpreter','LaTex')
+    scatter(T(1:ii*PlotSpeed),zk(1:ii*PlotSpeed,1),2,T(1:ii*PlotSpeed),'fill');
+    scatter(T(1:ii*PlotSpeed),zk(1:ii*PlotSpeed,2),2,T(1:ii*PlotSpeed),'fill');
+    scatter(T(1:ii*PlotSpeed),zk(1:ii*PlotSpeed,3),2,T(1:ii*PlotSpeed),'fill');
+    scatter(T(1:ii*PlotSpeed),zk(1:ii*PlotSpeed,4),2,T(1:ii*PlotSpeed),'fill');
+    colormap jet; caxis([0,Tp(end)]);
+    xlabel('$t$ [s]','Interpreter','LaTex');
+    ylabel(',\theta$','Interpreter','LaTex');
+    plot(T(ii*PlotSpeed),zk(ii*PlotSpeed,1),'o','LineWidth',1,'color','k');
+    plot(T(ii*PlotSpeed),zk(ii*PlotSpeed,2),'o','LineWidth',1,'color','k');
+    ax.FontSize = 14;
+    ax.TickLabelInterpreter = 'LaTex';
+    hold on; box on;
+    xlim([0,T(end)])
+    ylim([-max(max(abs(zk))),max(max(abs(zk)))])
+    
+    set(gcf,'Color','w')
+%     axis equal
+    drawnow    
+    
+    % % Save Animation
+    % frame = getframe(FigTag);
+    % im = frame2im(frame); 
+    % [imind,cm] = rgb2ind(im,256); 
+    % if ii==1
+    %  imwrite(imind,cm,filename,'gif','DelayTime',0.001, 'Loopcount',inf); 
+    % else 
+    %  imwrite(imind,cm,filename,'gif','DelayTime',0.001,'WriteMode','append'); 
+    % end         
+end
+    
