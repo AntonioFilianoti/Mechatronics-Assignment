7a32df4b785dc418b74e6a955b509f737aee5ae4
AntonioFilianoti <antonio.filianoti@mail.polimi.it>
2025-12-11 09:47:32 +0100

    added SImulink LQR assignment 1

--- Assignment 1/main.m
+++ Assignment 1/main.m
@@ -76,6 +76,7 @@ options_state = odeset('RelTol',1e-12,'AbsTol',1e-14);      % tolleranze ODE per
 Nmax = 5*10000;                         % numero massimo di iterazioni
 step = 5e-3;                            % passo di aggiornamento del controllo
 eps  = 5e-3;                            % soglia di convergenza
+eps = 0.1;
 u = [ones(1, Nsegment); zeros(1, Nsegment)];  % controllo iniziale (u1=1, u2=0)
 
 %% ------------------------- Procedura Iterativa --------------------------
@@ -234,81 +235,174 @@ for ii = 1:N % Note: try also reshape.m
     PolesC(ii,:) = eig(squeeze(Fc(ii,:,:)));    
 end
 
-FigTag = figure;
-FigPos = FigTag.Position;
-FigTag.Position = [FigPos(1)-FigPos(3)*0.25,FigPos(2)-FigPos(4)*0.2,FigPos(3)*1.5,FigPos(4)*1.2];
-filename = 'Trajectory.gif';
-PlotSpeed = 20;
-for ii = 1:N/PlotSpeed
+% FigTag = figure;
+% FigPos = FigTag.Position;
+% FigTag.Position = [FigPos(1)-FigPos(3)*0.25,FigPos(2)-FigPos(4)*0.2,FigPos(3)*1.5,FigPos(4)*1.2];
+% filename = 'Trajectory.gif';
+% PlotSpeed = 20;
+% for ii = 1:N/PlotSpeed
+% 
+%     clf(FigTag)
+% 
+%     % Temporal evolution - uncontrolled system
+%     ax = subplot(2,2,1); hold on; grid on;
+%     plot(real(PolesUC(ii*PlotSpeed,:)),imag(PolesUC(ii*PlotSpeed,:)),'x',...
+%                 'LineWidth',2,'Color','k'); hold on; box on;
+%     eigRe = real(PolesUC(1:ii*PlotSpeed,:));
+%     eigIm = imag(PolesUC(1:ii*PlotSpeed,:));
+%     Tmatrix = Tp(1:ii*PlotSpeed)*ones(1,size(eigRe,2));
+%     scatter(eigRe(:),eigIm(:),2,Tmatrix(:),'fill'); hold on; box on;
+%     colormap jet; caxis([0,Tp(end)]);
+%     title('Uncontrolled system','Interpreter','LaTex')
+%     xlabel('$\Re\{\lambda\}$ [Rad/s]','Interpreter','LaTex');
+%     ylabel('$\Im\{\lambda\}$ [Rad/s]','Interpreter','LaTex');
+%     ax.FontSize = 14;
+%     ax.TickLabelInterpreter = 'LaTex';
+%     ylim([-25,25])
+%     xlim([-50,10])
+% 
+%     % Temporal evolution - controlled system
+%     ax = subplot(2,2,2); hold on; grid on;
+%     plot(real(PolesC(ii*PlotSpeed,:)),imag(PolesC(ii*PlotSpeed,:)),'x',...
+%                 'LineWidth',2,'Color','k'); hold on; box on;
+%     eigRe = real(PolesC(1:ii*PlotSpeed,:));
+%     eigIm = imag(PolesC(1:ii*PlotSpeed,:));
+%     Tmatrix = Tp(1:ii*PlotSpeed)*ones(1,size(eigRe,2));
+%     scatter(eigRe(:),eigIm(:),2,Tmatrix(:),'fill'); hold on; box on;
+%     colormap jet;  caxis([0,Tp(end)]);
+%     title('Controlled system','Interpreter','LaTex')
+%     xlabel('$\Re\{\lambda\}$ [Rad/s]','Interpreter','LaTex');
+%     ylabel('$\Im\{\lambda\}$ [Rad/s]','Interpreter','LaTex');
+%     ax.FontSize = 14;
+%     ax.TickLabelInterpreter = 'LaTex';
+%     ylim([-25,25])
+%     xlim([-50,10])
+% 
+% 
+%     % plot of the trajectory
+%     ax = subplot(2,2,[3,4]);
+%     hold on; grid on;
+%     title('Trajectory','Interpreter','LaTex')
+%     scatter(T(1:ii*PlotSpeed),zk(1:ii*PlotSpeed,1),2,T(1:ii*PlotSpeed),'fill');
+%     scatter(T(1:ii*PlotSpeed),zk(1:ii*PlotSpeed,2),2,T(1:ii*PlotSpeed),'fill');
+%     scatter(T(1:ii*PlotSpeed),zk(1:ii*PlotSpeed,3),2,T(1:ii*PlotSpeed),'fill');
+%     scatter(T(1:ii*PlotSpeed),zk(1:ii*PlotSpeed,4),2,T(1:ii*PlotSpeed),'fill');
+%     colormap jet; caxis([0,Tp(end)]);
+%     xlabel('$t$ [s]','Interpreter','LaTex');
+%     ylabel(',\theta$','Interpreter','LaTex');
+%     plot(T(ii*PlotSpeed),zk(ii*PlotSpeed,1),'o','LineWidth',1,'color','k');
+%     plot(T(ii*PlotSpeed),zk(ii*PlotSpeed,2),'o','LineWidth',1,'color','k');
+%     ax.FontSize = 14;
+%     ax.TickLabelInterpreter = 'LaTex';
+%     hold on; box on;
+%     xlim([0,T(end)])
+%     ylim([-max(max(abs(zk))),max(max(abs(zk)))])
+% 
+%     set(gcf,'Color','w')
+% %     axis equal
+%     drawnow    
+% 
+%     % % Save Animation
+%     % frame = getframe(FigTag);
+%     % im = frame2im(frame); 
+%     % [imind,cm] = rgb2ind(im,256); 
+%     % if ii==1
+%     %  imwrite(imind,cm,filename,'gif','DelayTime',0.001, 'Loopcount',inf); 
+%     % else 
+%     %  imwrite(imind,cm,filename,'gif','DelayTime',0.001,'WriteMode','append'); 
+%     % end         
+% end
     
-    clf(FigTag)
-    
-    % Temporal evolution - uncontrolled system
-    ax = subplot(2,2,1); hold on; grid on;
-    plot(real(PolesUC(ii*PlotSpeed,:)),imag(PolesUC(ii*PlotSpeed,:)),'x',...
-                'LineWidth',2,'Color','k'); hold on; box on;
-    eigRe = real(PolesUC(1:ii*PlotSpeed,:));
-    eigIm = imag(PolesUC(1:ii*PlotSpeed,:));
-    Tmatrix = Tp(1:ii*PlotSpeed)*ones(1,size(eigRe,2));
-    scatter(eigRe(:),eigIm(:),2,Tmatrix(:),'fill'); hold on; box on;
-    colormap jet; caxis([0,Tp(end)]);
-    title('Uncontrolled system','Interpreter','LaTex')
-    xlabel('$\Re\{\lambda\}$ [Rad/s]','Interpreter','LaTex');
-    ylabel('$\Im\{\lambda\}$ [Rad/s]','Interpreter','LaTex');
-    ax.FontSize = 14;
-    ax.TickLabelInterpreter = 'LaTex';
-    ylim([-25,25])
-    xlim([-50,10])
-    
-    % Temporal evolution - controlled system
-    ax = subplot(2,2,2); hold on; grid on;
-    plot(real(PolesC(ii*PlotSpeed,:)),imag(PolesC(ii*PlotSpeed,:)),'x',...
-                'LineWidth',2,'Color','k'); hold on; box on;
-    eigRe = real(PolesC(1:ii*PlotSpeed,:));
-    eigIm = imag(PolesC(1:ii*PlotSpeed,:));
-    Tmatrix = Tp(1:ii*PlotSpeed)*ones(1,size(eigRe,2));
-    scatter(eigRe(:),eigIm(:),2,Tmatrix(:),'fill'); hold on; box on;
-    colormap jet;  caxis([0,Tp(end)]);
-    title('Controlled system','Interpreter','LaTex')
-    xlabel('$\Re\{\lambda\}$ [Rad/s]','Interpreter','LaTex');
-    ylabel('$\Im\{\lambda\}$ [Rad/s]','Interpreter','LaTex');
-    ax.FontSize = 14;
-    ax.TickLabelInterpreter = 'LaTex';
-    ylim([-25,25])
-    xlim([-50,10])
+%% SIMULINK LQR
+T = T';
+IC = [0;0; 0; 0];
+%K1 = squeeze(K);
+K1 = K(:,1,:);
+K1 = squeeze(K1);
+Wamp = 1; % amplitude of the disturbances
 
-    
-    % plot of the trajectory
-    ax = subplot(2,2,[3,4]);
-    hold on; grid on;
-    title('Trajectory','Interpreter','LaTex')
-    scatter(T(1:ii*PlotSpeed),zk(1:ii*PlotSpeed,1),2,T(1:ii*PlotSpeed),'fill');
-    scatter(T(1:ii*PlotSpeed),zk(1:ii*PlotSpeed,2),2,T(1:ii*PlotSpeed),'fill');
-    scatter(T(1:ii*PlotSpeed),zk(1:ii*PlotSpeed,3),2,T(1:ii*PlotSpeed),'fill');
-    scatter(T(1:ii*PlotSpeed),zk(1:ii*PlotSpeed,4),2,T(1:ii*PlotSpeed),'fill');
-    colormap jet; caxis([0,Tp(end)]);
-    xlabel('$t$ [s]','Interpreter','LaTex');
-    ylabel(',\theta$','Interpreter','LaTex');
-    plot(T(ii*PlotSpeed),zk(ii*PlotSpeed,1),'o','LineWidth',1,'color','k');
-    plot(T(ii*PlotSpeed),zk(ii*PlotSpeed,2),'o','LineWidth',1,'color','k');
-    ax.FontSize = 14;
-    ax.TickLabelInterpreter = 'LaTex';
-    hold on; box on;
-    xlim([0,T(end)])
-    ylim([-max(max(abs(zk))),max(max(abs(zk)))])
-    
-    set(gcf,'Color','w')
-%     axis equal
-    drawnow    
-    
-    % % Save Animation
-    % frame = getframe(FigTag);
-    % im = frame2im(frame); 
-    % [imind,cm] = rgb2ind(im,256); 
-    % if ii==1
-    %  imwrite(imind,cm,filename,'gif','DelayTime',0.001, 'Loopcount',inf); 
-    % else 
-    %  imwrite(imind,cm,filename,'gif','DelayTime',0.001,'WriteMode','append'); 
-    % end         
-end
-    
+
+% Gain = [T', K1];
+% size(K)
+% size(K1)
+
+%%
+
+sim('LQR_SImulink_Ass_1');
+
+Xs = squeeze(out.Xs.data);
+Xref = squeeze(out.Xref.data);
+U = squeeze(out.U.data);
+Uopt = squeeze(out.Uopt.data);
+time = squeeze(out.time.data);
+
+FigTag = figure;
+ax = axes;
+plot(time,Xref(1,:),'LineWidth',1,'LineStyle','--','color','b');
+hold on; grid on;
+plot(time,Xs(:,1),'LineWidth',0.5,'LineStyle','-','color','r');
+xlabel('$t$ [s]','Interpreter','LaTex')
+ylabel('$\dot{\theta}$ [rad/s]','Interpreter','LaTex')
+ax.FontSize = 14;
+ax.TickLabelInterpreter = 'LaTex';
+
+% print(FigTag,'fig3_1.jpeg','-djpeg','-r600')
+
+FigTag = figure;
+ax = axes;
+plot(time,Xref(2,:),'LineWidth',1,'LineStyle','--','color','b');
+hold on; grid on;
+plot(time,Xs(:,2),'LineWidth',0.5,'LineStyle','-','color','r');
+xlabel('Time [s]','Interpreter','LaTex')
+ylabel('$\theta$ [rad]','Interpreter','LaTex')
+ax.FontSize = 14;
+ax.TickLabelInterpreter = 'LaTex';
+
+% print(FigTag,'fig3_2.jpeg','-djpeg','-r600')
+
+FigTag = figure;
+ax = axes;
+plot(time,Uopt,'LineWidth',1,'LineStyle','--','color','b');
+hold on; grid on;
+plot(time,U,'LineWidth',0.5,'LineStyle','-','color','r');
+xlabel('$t$ [s]','Interpreter','LaTex')
+ylabel('$C$ [Nm]','Interpreter','LaTex')
+ax.FontSize = 14;
+ax.TickLabelInterpreter = 'LaTex';
+
+% print(FigTag,'fig3_3.jpeg','-djpeg','-r600')
+
+FigTag = figure;
+ax = axes;
+plot(time,Xs(:,1)'-Xref(1,:),'LineWidth',1,'LineStyle','-','Color','b');
+hold on; grid on;
+xlabel('$t$ [s]','Interpreter','LaTex')
+ylabel('$\dot{\delta\theta}$ [rad/s]','Interpreter','LaTex')
+ax.FontSize = 14;
+ax.TickLabelInterpreter = 'LaTex';
+
+% print(FigTag,'fig3_4.jpeg','-djpeg','-r600')
+
+FigTag = figure;
+ax = axes;
+plot(time,Xs(:,2)'-Xref(2,:),'LineWidth',1,'LineStyle','-','Color','b');
+hold on; grid on;
+xlabel('$t$ [s]','Interpreter','LaTex')
+ylabel('$\delta\theta$ [rad]','Interpreter','LaTex')
+ax.FontSize = 14;
+ax.TickLabelInterpreter = 'LaTex';
+
+% print(FigTag,'fig3_5.jpeg','-djpeg','-r600')
+
+FigTag = figure;
+ax = axes;
+plot(time,U-Uopt(:,1),'LineWidth',1,'LineStyle','-','Color','b');
+hold on; grid on;
+xlabel('$t$ [s]','Interpreter','LaTex')
+ylabel('$\delta C$ [Nm]','Interpreter','LaTex')
+legend({'$\delta c_1$','$\delta c_2$'},'Interpreter','Latex','location','best')
+ax.FontSize = 14;
+ax.TickLabelInterpreter = 'LaTex';
+drawnow
+
+% print(FigTag,'fig3_6.jpeg','-djpeg','-r600')
